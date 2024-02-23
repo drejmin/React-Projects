0e724f5976d31427d95510747abdcfe39f4baecd
drejmin <djjamn@gmail.com>
2024-02-23 13:13:57 -0800

    modified:   Pomodoro Timer/Pomodoro/package-lock.json 	modified:   Pomodoro Timer/Pomodoro/package.json 	renamed:    Pomodoro Timer/Pomodoro/src/assets/Relaxing Alarm tone stress free.mp3 -> Pomodoro Timer/Pomodoro/public/Relaxing Alarm tone stress free.mp3 	new file:   Pomodoro Timer/Pomodoro/public/logo.webp 	modified:   Pomodoro Timer/Pomodoro/src/pages/timer.tsx

--- Pomodoro Timer/Pomodoro/src/pages/timer.tsx
+++ Pomodoro Timer/Pomodoro/src/pages/timer.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button';
 import * as Select from '@radix-ui/react-select';
-import { useEffect, useState } from 'react';
-import '../assets/Relaxing Alarm tone stress free.mp3'
+import { useEffect, useRef, useState } from 'react';
+import '../../public/Relaxing Alarm tone stress free.mp3';
 
 function Timer() {
   const [secondsLeft, setSecondsLeft] = useState(60 * 25);
@@ -10,10 +10,11 @@ function Timer() {
   const [timerName, setTimerName] = useState('Pomodoro');
   const [alarmPlayed, setAlarmPlayed] = useState(false);
 
-  const alarmSound = new Audio('../assets/Relaxing Alarm tone stress free.mp3');
+  const alarmSound = useRef(new Audio('../../public/Relaxing Alarm tone stress free.mp3'))
+
   
   useEffect(() => {
-    
+
     let interval = null;
 
     if (isRunning && secondsLeft > 0) {
@@ -25,7 +26,7 @@ function Timer() {
     }
 
     if (secondsLeft ===0 && !alarmPlayed){
-        alarmSound.play();
+        alarmSound.current.play().catch(error=> console.log(error));
         setAlarmPlayed(true);
     }
 
@@ -58,6 +59,15 @@ function Timer() {
     }
   };
 
+  const handleStopMusic = () =>{
+    if (alarmSound.current) {
+        alarmSound.current.pause(); // Stop the audio playback
+        alarmSound.current.currentTime = 0; // Reset audio position to start
+      }
+      setAlarmPlayed(false); // Reset alarm played state
+    };
+  
+
   return (
     <div  className='common-container flex flex-1 justify-center items-center flex-col mt-10 py-10'>
       <h2 className='h1-bold md: title-bold text-center w-full'>{timerName}: {Math.floor(secondsLeft / 60)}:{String(secondsLeft % 60).padStart(2, '0')}</h2>
@@ -67,10 +77,11 @@ function Timer() {
             <Select.Value placeholder='Select a Interval'>{timerName}▾</Select.Value>
             </Select.Trigger>
             <Select.Content>
-            <Select.Viewport className='bg: to-black flex flex-col h3-bold md: h2-bold text-center'>
+            <Select.Viewport className='flex flex-col h3-bold md: h2-bold text-center'>
                 <Select.Item className='mt-8' value="1500">Pomodoro</Select.Item>
                 <Select.Item className='mt-1.5' value="300">Short Break</Select.Item>
                 <Select.Item className='mt-1.5' value="900">Long Break</Select.Item>
+                <Select.Item className='mt-1.5' value="10">test</Select.Item>
             </Select.Viewport>
             </Select.Content>
         </Select.Root>
@@ -78,10 +89,9 @@ function Timer() {
       <div className='flex flex-row padding:2 mt-4 whitespace-nowrap'>
       <Button className='h3-bold' onClick={() => setIsRunning(true)} disabled={isRunning || secondsLeft === 0}>Start</Button>
       <Button className='h3-bold' onClick={() => setIsRunning(false)} disabled={!isRunning}>Pause</Button>
-      <Button className='h3-bold' onClick={() => { setSecondsLeft(initialTime); setIsRunning(false); }}>Reset</Button>
+      <Button className='h3-bold' onClick={() => { handleStopMusic(); setSecondsLeft(initialTime); setIsRunning(false); setAlarmPlayed(false)}}>Reset</Button>
       </div>
     </div>
   );
 }
-
 export default Timer;
